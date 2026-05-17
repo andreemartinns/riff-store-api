b83413d2975d487c407c7cb6ad73470918697326
andreemartinns <andreemartinns2@gmail.com>
2026-05-17 13:35:45 -0300

    .

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import 'dotenv/config'
 
 import express from 'express'
 
+import cors from 'cors'
+
 import authRoutes from './routes/auth.routes.js'
 import productRoutes from './routes/product.routes.js'
 import categoryRoutes from './routes/category.routes.js'
@@ -9,6 +11,7 @@ import orderRoutes from './routes/order.routes.js'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/auth', authRoutes)
